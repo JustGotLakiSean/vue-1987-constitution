84f24af1bb9ee13ec12f5838bdaf6935f7fe125b
justgotlakisean <joshua222.98@gmail.com>
2026-03-16 10:30:11 +0800

    added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref} from 'vue'
+const headerTitle = ref('1987 CONSTITUTION OF THE REPUBLIC OF THE PHILIPPINES')
+</script>
+
+<template>
+    <header>
+        <h1>{{ headerTitle }}</h1>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
